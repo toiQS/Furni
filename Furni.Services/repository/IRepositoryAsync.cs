1439bc8d5645b15a22a93922015bdf8c92b52d3f
Akai <nguyensieu12112002@gmail.com>
2024-09-04 09:14:14 +0700

    Adding repository

---
+++ Furni.Services/repository/IRepositoryAsync.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Furni.Services.repository
+{
+    public interface IRepositoryAsync<T>
+    {
+        public Task<IEnumerable<T>> GetValuesAsync();
+        public Task<bool> Create(T entity);
+        public Task<bool> Delete(T entity);
+        public Task<bool> Update(T entity);
+        public string GetPathFolderCurrent();
+        public string GetFolderName(string folderName);
+        public string GetFileName(string fileName);
+        public Task LogErrorAsync(string path, Exception ex);
+    }
+}
