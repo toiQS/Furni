685ddfa86ac7c51df090e131ce0cd86ed0c53d92
Akai <nguyensieu12112002@gmail.com>
2024-09-06 08:30:50 +0700

    Add Web App API

---
+++ Furni.Data/Migrations/20240905093638_Update-modifly-attribute-item.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Furni.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Updatemodiflyattributeitem : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<float>(
+                name: "Total",
+                table: "Item",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Total",
+                table: "Item");
+        }
+    }
+}
