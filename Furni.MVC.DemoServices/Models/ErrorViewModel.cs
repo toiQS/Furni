d6981dd17abe570ed800d118031edacbf846e6aa
Akai <nguyensieu12112002@gmail.com>
2024-09-10 16:13:04 +0700

    add mvc demo services

---
+++ Furni.MVC.DemoServices/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Furni.MVC.DemoServices.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
