102c95a2e9853e0f751dbf9cde57b8685f354203
Akai <nguyensieu12112002@gmail.com>
2024-09-11 00:32:32 +0700

    Update BlogController.cs

--- Furni.API/Controllers/BlogController.cs
+++ Furni.API/Controllers/BlogController.cs
@@ -8,7 +8,7 @@ using System.Collections.Generic;
 
 namespace Furni.API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class BlogController : ControllerBase
     {
