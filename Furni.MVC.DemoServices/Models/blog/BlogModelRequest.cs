714cce314a6db10601e0fcd5abbb4a9836007a5e
Akai <nguyensieu12112002@gmail.com>
2024-09-11 16:09:26 +0700

    add model

---
+++ Furni.MVC.DemoServices/Models/blog/BlogModelRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Furni.MVC.DemoServices.Models.blog
+{
+    public class BlogModelRequest
+    {
+        public string BlogName { get; set; } = string.Empty;
+        public string UserIdCreated { get; set; } = string.Empty;
+    }
+}
---
+++ Furni.MVC.DemoServices/Models/blog/BlogModelResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace Furni.MVC.DemoServices.Models.blog
+{
+    public class BlogModelResponse
+    {
+        public string BlogId { get; set; } = string.Empty;
+        public string BlogName { get; set; } = string.Empty;
+        public string UserIdCreated { get; set; } = string.Empty;
+        public DateTime CreateAt { get; set; }
+        public DateTime UpdateAt { get; set; }
+    }
+}
